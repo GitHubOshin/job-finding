292a1cbef6672122439a22314a0a48b4aa574ad0
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-11-29 01:47:02 +0700

    Update HowPlatformWorks.js

---
+++ src/data/HowPlatformWorks.js
@@ -0,0 +1,35 @@
+const howPlatformWorks = [
+  {
+    id: 1,
+    alternative: 'Profile Icon',
+    icon: 'images/icons/profile.svg',
+    title: 'Create Account',
+    detail:
+      'Competently foster backward-compatible alignments and multimedia based resources.'
+  },
+  {
+    id: 2,
+    alternative: 'Upload Icon',
+    icon: 'images/icons/paper-upload.svg',
+    title: 'Upload CV/Resume',
+    detail:
+      'Competently foster backward-compatible alignments and multimedia based resources.'
+  },
+  {
+    id: 3,
+    alternative: 'Search Icon',
+    icon: 'images/icons/search.svg',
+    title: 'Choose Job',
+    detail:
+      'Competently foster backward-compatible alignments and multimedia based resources.'
+  },
+  {
+    id: 4,
+    alternative: 'Check Icon',
+    icon: 'images/icons/check.svg',
+    title: 'Complete Application',
+    detail:
+      'Competently foster backward-compatible alignments and multimedia based resources.'
+  }
+]
+export default howPlatformWorks
