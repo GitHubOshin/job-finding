978970b1057f869068b51eabc9b5fcdd7361a8b0
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-02 11:01:17 +0700

    Update FirstPage.js

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,5 +1,17 @@
+import HeaderFirstPage from '../components/firstPage/HeaderFirstPage'
+import SearchingFirstPage from '../components/firstPage/SearchingFirstPage'
+import PeopleHaveFoundJobFirstPage from '../components/firstPage/PeopleHaveFoundJobFirstPage'
+
 function FirstPage() {
-  return <div className=""></div>
+  return (
+    <div className="bg-[#F9FDFD] laptop:w-[1920px] laptop:h-[960px] mobile:w-[375px] mobile:h-[703.58px] laptop:flex laptop:flex-col mobile:flex mobile:flex-col mobile:justify-between mobile:items-center">
+      <HeaderFirstPage />
+      <div className="laptop:flex">
+        <SearchingFirstPage />
+        <PeopleHaveFoundJobFirstPage />
+      </div>
+    </div>
+  )
 }
 
 export default FirstPage
